8a453119951f01ccd47bd0da53af03f8dff39502
Maia Teegarden <padmaia.thedev@gmail.com>
2018-08-24 19:23:20 -0700

    nuke it and start over again... again :construction:
    for real this time

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**',
-    '!src/**/*.spec.*',
-    '!**/dev.*',
-    '!src/client/index.jsx',
-  ],
-};
--- src/server/index.js
+++
@@ -1,5 +0,0 @@
-import server from './server';
-
-server.listen(3000, () => {
-  console.log('THE DUMP is listening on port 3000!'); // eslint-disable-line no-console
-});
